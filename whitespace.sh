e06cfb15115ffa892f11bb795d90c44ac15a9a5e
Maciek Sakrejda <m.sakrejda@gmail.com>
2010-12-21 18:48:41 -0800

    Better whitespace cleanup (trims trailing whitespace)

--- whitespace.sh
+++ whitespace.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find \( -name \*.as -o -name \*.mxml \) -print0 | xargs -0 sed -i -r 's/\t/    /g'
+find \( -name \*.as -o -name \*.mxml \) -print0 | xargs -0 sed -i -r -e 's/\t/    /g' -e 's/\s+$//'
